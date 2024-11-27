f2e45df93fcb7298d5084bcd30332f27da99a791
Darya Melgunova <darya.melgunova@gmail.com>
2024-11-27 11:41:24 +0300

    added build.gradle

--- src/test/java/SearchTestGit.java
+++ src/test/java/SearchTestGit.java
@@ -11,12 +11,25 @@ public class SearchTestGit {
 
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $("input#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-filter").setValue("SoftAssertions");
         $(".wiki-rightbar").shouldHave(text("SoftAssertions"));
         $(".wiki-rightbar").$(byText("SoftAssertions")).click();
-        $(".markdown-heading.heading-element").shouldHave(text("3. Using JUnit5 extend test class:"));
-
-
+        $$(".markdown-heading .heading-element")
+                .findBy(text("3. Using JUnit5 extend test class:"))
+                .parent()
+                .sibling(0)
+                .shouldHave(text(
+                        "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "    \n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"));
 
     }
 }
